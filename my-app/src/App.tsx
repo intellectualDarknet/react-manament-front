52b5ed663c339f674ff45e582f6a3bad607d3bbd
intellectualDarknet <ladyblaumeux24@gmail.com>
2022-11-10 14:03:36 +0300

    feat: add router

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="Start" element={<Start />} />
+          <Route path="Authorization" element={<Authorization />} />
+          <Route path="Boards" element={<Boards />} />
+          <Route path="404" element={<Error />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

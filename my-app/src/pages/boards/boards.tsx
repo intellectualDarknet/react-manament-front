56e4aca4cb2eab93360f944cd089edbfc4d0e622
veronicavoevodina <veranika1711@gmail.com>
2022-11-15 15:58:14 +0300

    feat: add container

--- my-app/src/pages/boards/boards.tsx
+++ my-app/src/pages/boards/boards.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  margin: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Boards = () => {
-  return <></>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid xs={2} sm={4} md={4} key={index}>
+            <Item sx={{ height: '200px' }}>board name</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Boards;
